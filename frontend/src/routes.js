df962d891b4e65d8231dc8304252f9efe5a25b0d
ajharh9 <ajharh999@gmail.com>
2024-12-01 12:34:39 +0530

    added ProjectPage

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,10 +1,12 @@
 import HomePage from "./pages/HomePage"
 import AboutPage from "./pages/AboutPage"
 import ContactPage from "./pages/ContactPage"
+import ProjectPage from "./pages/ProjectPage";
 
 const routes = [
         { path:'/', component:HomePage },
         { path:'/about', component:AboutPage },
+        { path:'/project', component:ProjectPage },
         { path:'/contact', component:ContactPage },
     ];
 
